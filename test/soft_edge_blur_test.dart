464e58959da964fb317435b16dd40b0d7b535b02
Raouf Rahiche <mrrahicheraouf@gmail.com>
2024-09-10 23:01:11 +0100

    add repository

--- test/soft_edge_blur_test.dart
+++ test/soft_edge_blur_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:soft_edge_blur/soft_edge_blur.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+void main() {}
